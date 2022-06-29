fd7e6d2c3a5b5266a035902af345cb4fe28b5db1
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-06-29 19:40:35 +0530

    Hello World Program in Java

---
+++ HelloWorld/src/com/bridgelabz/demoprogram/Demomain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.demoprogram;
+
+public class Demomain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
